8ad5316d17112a7d6379bc855ad9a68814376e66
Grisha Temchenko <soid.exe@gmail.com>
2020-08-30 21:28:55 -0400

    download nltk punkt data set in training scripts

--- scripts/wiki_search_train.py
+++ scripts/wiki_search_train.py
@@ -1,8 +1,10 @@
 # Classify wiki search results for which are to match / not-match or needs checking the article text.
 
+import nltk
 from cu_catalog.models.wiki_search import WikiSearchClassifier
 
 if __name__ == '__main__':
+    nltk.download('punkt')
     model = WikiSearchClassifier()
     model.load_training_data()
     model.fit()
